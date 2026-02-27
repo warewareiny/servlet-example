61303c8b1b15097353960398b838deab0f954695
Tatyana Zagaynova <warewareiny@gmail.com>
2026-02-27 21:53:59 +0300

    Add Payment entity

--- src/main/java/org/example/carrentalproject/entity/Payment.java
+++ src/main/java/org/example/carrentalproject/entity/Payment.java
@@ -1,4 +1,23 @@
 package org.example.carrentalproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Payment {
+
+    private Long idPayment;
+    private Long idOrder;
+    private BigDecimal amount;
+    private String paymentMethod;
+    private String paymentStatus;
+    private LocalDateTime paymentDate;
 }
