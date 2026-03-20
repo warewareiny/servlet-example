b9b1166f448cb27cadc0d0199254ab239cebf83d
Tatyana Zagaynova <warewareiny@gmail.com>
2026-03-20 23:50:31 +0300

    add: CarDaoTest with empty methods

---
+++ src/test/java/org/example/car/rental/project/dao/CarDaoTest.java
@@ -0,0 +1,61 @@
+package org.example.car.rental.project.dao;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class CarDaoTest {
+
+    @Test
+    @Tag("delete")
+    void shouldReturnTrueIfDeleteExistingCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("delete")
+    void shouldReturnFalseIfDeleteIncorrectCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("findAll")
+    void shouldReturnAllCars() {
+//        todo
+    }
+
+    @Test
+    @Tag("save")
+    void shouldSaveCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldFindByIdExistingCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldReturnEmptyOptionalIfFindByIdIncorrectCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldUpdateStatus() {
+//        todo
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldReturnTrueIfUpdateExistingCar() {
+//        todo
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldDoNothingIfUpdateNotExistingCar() {
+//        todo
+    }
+}
