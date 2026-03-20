9ff3441551d1a3d236566891a614e4e2bfd73314
Tatyana Zagaynova <warewareiny@gmail.com>
2026-03-20 23:53:16 +0300

    add: ClientDaoTest with empty methods

---
+++ src/test/java/org/example/car/rental/project/dao/ClientDaoTest.java
@@ -0,0 +1,25 @@
+package org.example.car.rental.project.dao;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ClientDaoTest {
+
+    @Test
+    @Tag("save")
+    void shouldSaveClient() {
+//        todo
+    }
+
+    @Test
+    @Tag("findByEmailAndPassword")
+    void shouldReturnExistingPersonWithCorrectData() {
+//        todo
+    }
+
+    @Test
+    @Tag("findByEmailAndPassword")
+    void shouldReturnEmptyOptionalIfPasswordOrEmailIsIncorrect() {
+//        todo
+    }
+}
